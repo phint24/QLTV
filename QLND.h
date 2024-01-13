119c81bceead2251d338f5649b16e379b8c588b6
phint24 <nguyentanphi24022005@gmail.com>
2024-01-13 21:03:18 +0700

    qltv

---
+++ QLND.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Header.h"
+#include <string>
+#include <vector>
+
+//cac ham
+void nhap2(int& input);
+void TiepTuc(int& n);
+void ThemNgDung();
+void SuaTTNgDung();
+void XoaNgDung();
+void HienThiNgDung();
+void QLNgDung();
---
+++ QLTV.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "Header.h"
+#include <string>
+#include <vector>
+
+//cac ham 
+void nhap(int& input);
+void TiepTuc(int &n);
+void ThemSach();
+void SuaTTSach();
+void XoaSach();
+void HienThi();
+void QuanLiTV();
+void nhap3(int& input);
+void Timkiem_tensach();
+void Timkiem_masach();
+void Timkiem_chude();
+void Timkiem_tacgia();
+void User();
