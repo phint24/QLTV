2ef4bedf0d6d8fb255e67eb0b37e8fed8ba45822
phint24 <155838710+phint24@users.noreply.github.com>
2024-01-15 16:12:09 +0700

    Add files via upload

---
+++ User.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "Header.h"
+#include "QLTV.h"
+
+void nhap3(int &input);
+void Timkiem_tensach();
+void Timkiem_masach();
+void Timkiem_chude();
+void Timkiem_tacgia();
+void User();
